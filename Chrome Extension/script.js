8631e9c2cd006fc554bf5bcb3eeecadd35570c08
Caspar Stählin <94377516+chessper53@users.noreply.github.com>
2023-03-01 12:27:39 +0100

    Update script.js

--- Chrome Extension/script.js
+++ Chrome Extension/script.js
@@ -1,12 +1,15 @@
+//Gets the Input Element
 document.getElementById("pwgenerator").addEventListener("click", generatepassword);
 
 function generatepassword(){
-
+    //Check for the clicked Radio
     if(document.getElementById('compRandom').checked) {
         let result = '';
         let length = 9;
+        //Define Range of Characters
         let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
         let charactersLength = characters.length;
+        //Generate the Random Characters
         for ( let i = 0; i < length; i++ ) {
             result += characters.charAt(Math.floor(Math.random() * charactersLength));
         }
@@ -14,8 +17,10 @@ function generatepassword(){
     }else if(document.getElementById('specialchars').checked) {
         let result = '';
         let length = 9;
+        //Define Range of Characters
         let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789$-/%@?!';
         let charactersLength = characters.length;
+        //Generate the Random Characters
         for ( let i = 0; i < length; i++ ) {
             result += characters.charAt(Math.floor(Math.random() * charactersLength));
         }
